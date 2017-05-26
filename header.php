5d33b8cc1e6face91164fba76fe2231774a62942
Mike Oberdick <mikeoberdick@users.noreply.github.com>
2017-05-26 17:41:59 -0400

    Added Favicon for all devices

--- header.php
+++ header.php
@@ -15,12 +15,14 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="<?php echo get_stylesheet_directory_uri() . '/img/favicon.ico';?>" />
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
+	
 </head>
 
 <body <?php body_class(); ?>>
