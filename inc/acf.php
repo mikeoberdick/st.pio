5bf79983a63c8771dde66a8f004fb5221e10dd87
Mike Oberdick <mikeoberdick@users.noreply.github.com>
2017-06-26 08:35:46 -0400

    Changed Company Info to Company Profile

--- inc/acf.php
+++ inc/acf.php
@@ -11,10 +11,10 @@ if( function_exists('acf_add_options_page') ) {
     $args = array(
     
     /* (string) The title displayed on the options page. Required. */
-    'page_title' => 'Company Info',
+    'page_title' => 'Company Profile',
     
     /* (string) The title displayed in the wp-admin sidebar. Defaults to page_title */
-    'menu_title' => 'Company Info',
+    'menu_title' => 'Company Profile',
     
 );
 
