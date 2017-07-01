d4e6a0c3f3d6722571709707efa6aa832404a9f2
Mike Oberdick <mikeoberdick@users.noreply.github.com>
2017-07-01 09:16:32 -0400

    Alpa order

--- functions.php
+++ functions.php
@@ -1,19 +1,9 @@
 <?php
 /**
- * WP Understrrap Functions and Definitions
+ * D4TW Understrap Child Theme Functions and Definitions
  *
  * @package understrap
  */
-
-/**
- * Theme setup
- */
-require get_stylesheet_directory() . '/inc/setup.php';
-
-/**
- * Setup Custom Dashboard
- */
-require get_stylesheet_directory() . '/inc/dashboard.php';
 
 /**
  * Advanced Custom Fields
@@ -21,21 +11,19 @@ require get_stylesheet_directory() . '/inc/dashboard.php';
 require get_stylesheet_directory() . '/inc/acf.php';
 
 /**
- * Theme Menus & Custom Functionality
+ * Setup Custom Post Types
+ */
+require get_stylesheet_directory() . '/inc/cpt.php';
+
+/**
+ * Setup Dashboard
+ */
+require get_stylesheet_directory() . '/inc/dashboard.php';
+
+/**
+ * Setup Theme Menus
  */
 require get_stylesheet_directory() . '/inc/menus.php';
-
-/**
- * Advanced Custom Fields
- */
-require get_stylesheet_directory() . '/inc/woocommerce.php';
-
-/**
- * Register theme styles
- *
- * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
- */
-require get_stylesheet_directory() . '/inc/styles.php';
 
 /**
  * Register theme scripts
@@ -45,9 +33,40 @@ require get_stylesheet_directory() . '/inc/styles.php';
 require get_stylesheet_directory() . '/inc/scripts.php';
 
 /**
+ * Theme setup
+ */
+require get_stylesheet_directory() . '/inc/setup.php';
+
+/**
+ * Register theme styles
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+ */
+require get_stylesheet_directory() . '/inc/styles.php';
+
+/**
+ * Register Users
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+ */
+require get_stylesheet_directory() . '/inc/users.php';
+
+/**
  * Customize widget areas
  *
  * @link https://codex.wordpress.org/Function_Reference/unregister_sidebar
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 require get_stylesheet_directory() . '/inc/widgets.php';
+
+/**
+ * Woocommerce
+ */
+require get_stylesheet_directory() . '/inc/woocommerce.php';
+
+/**
+ * Custom template tags
+ *
+ * @link https://codex.wordpress.org/Template_Tags
+ */
+require get_stylesheet_directory() . '/inc/template-tags.php';
