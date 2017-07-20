f201d652180082b192e9b4d841f5dee1d59c0614
Mike Oberdick <mikeoberdick@users.noreply.github.com>
2017-07-20 11:47:03 -0400

    Added Code for Forcing CSS/JS Refresh

--- inc/setup.php
+++ inc/setup.php
@@ -22,8 +22,8 @@ function theme_enqueue_styles() {
 	// Get the theme data
 	$the_theme = wp_get_theme();
 
-    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), filemtime( get_stylesheet_directory() . '/css/child-theme.min.css' ) );
+    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), filemtime( get_stylesheet_directory() . '/css/child-theme.min.css' ), true );
 }
 
 //Allow Text widgets to execute shortcodes
