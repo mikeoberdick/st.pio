d8746521429c18772c273097f31ab5ed154f3440
Mike Oberdick <mikeoberdick@users.noreply.github.com>
2017-07-23 05:32:50 -0400

    Tightened Up Code

--- inc/acf.php
+++ inc/acf.php
@@ -8,16 +8,10 @@
 //Add the ACF options page
 if( function_exists('acf_add_options_page') ) {
 
-    $args = array(
-    
-    /* (string) The title displayed on the options page. Required. */
-    'page_title' => 'Company Profile',
-    
-    /* (string) The title displayed in the wp-admin sidebar. Defaults to page_title */
-    'menu_title' => 'Company Profile',
-    
-);
-
-    acf_add_options_page( $args );
+	acf_add_options_page(array(
+		'page_title' 	=> 'Company Profile',
+		'menu_title'	=> 'Company Profile',
+		'menu_slug' 	=> 'company-profile'
+	));
     
 }
