bff7641718ae096b98bfcd35fd7e49c71058cd14
Mike Oberdick <mikeoberdick@users.noreply.github.com>
2017-10-29 08:53:49 -0400

    Added Google Maps API

--- inc/acf.php
+++ inc/acf.php
@@ -15,3 +15,17 @@ if( function_exists('acf_add_options_page') ) {
 	));
     
 }
+
+//Register the Google Maps API for use with ACF
+function google_maps_scripts() {
+		wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCxL_4g2Eo8oafO2EVNKa6q8AOKL0U8mxE', array(), '3', true );
+		wp_enqueue_script( 'google-map-init', get_stylesheet_directory_uri() . '/js/google-maps.js', array('google-map', 'jquery'), '0.1', true );
+	}
+add_action( 'wp_enqueue_scripts', 'google_maps_scripts' );
+
+function my_acf_google_map_api( $api ){
+	$api['key'] = 'AIzaSyCxL_4g2Eo8oafO2EVNKa6q8AOKL0U8mxE';
+	return $api;	
+}
+
+add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
