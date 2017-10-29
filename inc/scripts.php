78db8ddf9327ecfe126eab9e07b380472573439b
Mike Oberdick <mikeoberdick@users.noreply.github.com>
2017-10-29 09:10:10 -0400

    Removed ACF Map Scripts to Move to ACF Page

--- inc/scripts.php
+++ inc/scripts.php
@@ -9,10 +9,5 @@
 function d4tw_enqueue_scripts () {
    wp_enqueue_script( 'D4TW Theme JS', get_stylesheet_directory_uri() . '/js/d4tw.js', array('jquery'), '1.0.0', true );
    wp_enqueue_script( 'AOS JS', get_stylesheet_directory_uri() . '/aos/aos.js', array('jquery'), '1.0.0', true );
-
-   if (is_page('contact')) {
-   		wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key=XXXXX', array(), '3', true );
-		wp_enqueue_script( 'google-map-init', get_template_directory_uri() . '/library/js/google-maps.js', array('google-map', 'jquery'), '0.1', true );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'd4tw_enqueue_scripts' );
